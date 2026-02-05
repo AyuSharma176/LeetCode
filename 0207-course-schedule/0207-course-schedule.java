2a46689b52381f9362e2b663cf10a8223f1f82ee
Ayush Sharma <140237136+AyuSharma176@users.noreply.github.com>
2026-02-05 19:44:24 +0530

    Time: 11 ms (11.92%), Space: 47.2 MB (62.41%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,51 +1,44 @@
-import java.util.*;
-
 class Solution {
-    private int topoBFS(int V, List<List<Integer>> adj) {
-        int[] indegree = new int[V];
+    public boolean canFinish(int numCourses, int[][] p) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
 
-        for (int i = 0; i < V; i++) {
-            for (int nbr : adj.get(i)) {
+        for(int[] pre: p){
+            int course=pre[0];
+            int req=pre[1];
+            adj.get(req).add(course);
+        }
+        int completed=topo(numCourses,adj);
+        return completed==numCourses;
+    }
+    public int topo(int v,List<List<Integer>> adj){
+        int[] indegree=new int[v];
+        for(int i=0;i<v;i++){
+            for(int nbr: adj.get(i)){
                 indegree[nbr]++;
             }
         }
-
         Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < V; i++) {
-            if (indegree[i] == 0) {
+        for(int i=0;i<v;i++){
+            if(indegree[i]==0){
                 q.add(i);
             }
         }
+        int count =0;
+        while(!q.isEmpty()){
+            int rv = q.poll();
 
-        int count = 0;
-        while (!q.isEmpty()) {
-            int node = q.poll();
             count++;
 
-            for (int nbr : adj.get(node)) {
+            for(int nbr: adj.get(rv)){
                 indegree[nbr]--;
-                if (indegree[nbr] == 0) {
+                if(indegree[nbr]==0){
                     q.add(nbr);
                 }
             }
         }
-
-        return count; 
-    }
-
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());
-        }
-
-        for (int[] pre : prerequisites) {
-            int course = pre[0];
-            int prereq = pre[1];
-            adj.get(prereq).add(course);
-        }
-        int completed = topoBFS(numCourses, adj);
-
-        return completed == numCourses;
+        return count;
     }
 }
