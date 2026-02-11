268cee1c2b8c5b81e4cf3c13f3038b9ea520b506
Ayush Sharma <140237136+AyuSharma176@users.noreply.github.com>
2026-02-11 22:00:33 +0530

    Time: 7 ms (99.37%), Space: 47.4 MB (6.37%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -3,15 +3,14 @@ class Solution {
         int n=nums.length;
         int m=queries.length;
         Arrays.sort(nums);
-        int[] sum = new int[n+1];
-        for(int i=0;i<n;i++) sum[i+1]=sum[i]+nums[i];
-        int[] ans = new int[m];
+        for(int i = 1; i < n; i++) nums[i] += nums[i - 1];
+        
 
         for(int i=0;i<m;i++){
-            int index= Arrays.binarySearch(sum,queries[i]+1);
+            int index= Arrays.binarySearch(nums,queries[i]+1);
             if(index<0) index= ~index;
-            ans[i]=index-1;
+            queries[i]=index;
         }
-        return ans;
+        return queries;
     }
 }
