ce904e6255e46fc150d0b1f23c43237a22f1dc8f
Ayush Sharma <140237136+AyuSharma176@users.noreply.github.com>
2026-03-10 15:41:14 +0530

    Time: 7 ms (98.7%), Space: 14.1 MB (79.26%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,35 +1,19 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int n=nums.size();
-        int totalsum=0;
-        for(int i=0;i<nums.size();i++){
-            totalsum+=nums[i];
-        }
-        if(totalsum%2!=0){
+    static bool canPartition(const vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % 2 == 1) {
             return false;
         }
-        int target=totalsum/2;
-        vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        return solve(nums,target,0,dp);
-
-    }
-    bool solve(vector<int>& nums,int target,int i,vector<vector<int>>& dp){
-        if(i>=nums.size()){
+        int s = sum / 2;
+        bitset<20001> dp;
+        dp[0] = 1;
+        for (int i = 0; i < nums.size(); ++i){
+            dp |= dp << nums[i];
+        }
+        if (dp[s] == 0) {
             return false;
         }
-        if(target==0){
-            return true;
-        }
-        if(dp[i][target]!=-1){
-            return dp[i][target];
-        }
-        bool inc=false;
-        if(nums[i]<=target){
-            inc=solve(nums,target-nums[i],i+1,dp);
-        }
-        bool ninc=solve(nums,target,i+1,dp);
-
-        return dp[i][target]=inc||ninc;
+        return true;
     }
 };
