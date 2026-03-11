38171958f92c15afd8b9319c79389737aeea0575
Ayush Sharma <140237136+AyuSharma176@users.noreply.github.com>
2026-03-11 11:16:09 +0530

    Time: 4 ms (8.53%), Space: 12.7 MB (5.73%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& nums, int k) {
-       queue<pair<int,bool>> q;
-    //    bool ok=false;
-    //    if(k==0){
-    //     ok=true;
-    //    }
-    //    q.add({nums[0],ok});
-       for(int i=0;i<nums.size();i++){
-        bool ok=false;
-        if(i==k){
-            ok=true;
+        queue<pair<int, bool>> q;
+        for (int i = 0; i < nums.size(); i++) {
+            bool ok = false;
+            if (i == k) {
+                ok = true;
+            }
+            q.push({nums[i], ok});
         }
-        q.push({nums[i],ok});
-       }
-       int sum=0;
-       while(!q.empty()){
-        auto rv=q.front();
-        int f=rv.first;
-        bool s= rv.second;
-        q.pop();
-        f=f-1;
-        sum+=1;
-        if(s && f==0){
-            return sum;
+        int sum = 0;
+        while (!q.empty()) {
+            auto rv = q.front();
+            int f = rv.first;
+            bool s = rv.second;
+            q.pop();
+            f = f - 1;
+            sum += 1;
+            if (s && f == 0) {
+                return sum;
+            }
+            if (f == 0 && !s) {
+                continue;
+            } else {
+                q.push({f, s});
+            }
         }
-        if(f==0 && !s){
-            continue;
-        }
-        else{
-            q.push({f,s});
-        }
-       }
-       return sum;
+        return sum;
     }
 };
