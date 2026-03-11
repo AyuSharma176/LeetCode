bf1267c39d8ee5afb7652113c738458c57db3cc4
Ayush Sharma <140237136+AyuSharma176@users.noreply.github.com>
2026-03-11 11:19:41 +0530

    Time: 0 ms (100%), Space: 10.5 MB (62.98%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& nums, int k) {
-       int i=0;
-       int n=nums.size();
-       int time=0;
+        int i = 0;
+        int n = nums.size();
+        int time = 0;
 
-       while(nums[k]>0){
-        if(nums[i]>0){
-            nums[i]--;
-            time++;
+        while (nums[k] > 0) {
+            if (nums[i] > 0) {
+                nums[i]--;
+                time++;
+            }
+            if (nums[k] == 0) {
+                break;
+            }
+
+            i = (i + 1) % n;
         }
-        if(nums[k]==0){
-            break;
-        }
-        
-        i=(i+1)%n;
-       }
-       return time;
+        return time;
     }
 };
