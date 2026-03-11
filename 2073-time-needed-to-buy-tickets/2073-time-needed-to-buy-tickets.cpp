c72c3753dd836d3a6650df1f368ce7f853e10e24
Ayush Sharma <140237136+AyuSharma176@users.noreply.github.com>
2026-03-11 11:19:36 +0530

    Time: 0 ms (100%), Space: 10.6 MB (42.99%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& nums, int k) {
-        queue<pair<int, bool>> q;
-        for (int i = 0; i < nums.size(); i++) {
-            bool ok = false;
-            if (i == k) {
-                ok = true;
-            }
-            q.push({nums[i], ok});
+       int i=0;
+       int n=nums.size();
+       int time=0;
+
+       while(nums[k]>0){
+        if(nums[i]>0){
+            nums[i]--;
+            time++;
         }
-        int sum = 0;
-        while (!q.empty()) {
-            auto rv = q.front();
-            int f = rv.first;
-            bool s = rv.second;
-            q.pop();
-            f = f - 1;
-            sum += 1;
-            if (s && f == 0) {
-                return sum;
-            }
-            if (f == 0 && !s) {
-                continue;
-            } else {
-                q.push({f, s});
-            }
+        if(nums[k]==0){
+            break;
         }
-        return sum;
+        
+        i=(i+1)%n;
+       }
+       return time;
     }
 };
