743e6f5f17c46f841c99393424d04a3827d8dbb8
Ayush Sharma <140237136+AyuSharma176@users.noreply.github.com>
2026-03-12 21:19:26 +0530

    Time: 13 ms (3.07%), Space: 28.2 MB (6.9%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,29 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
         set<int> set;
-        kth(root,set);
-        for(int x:set){
-            // cout<<x;
-            k--;
-            if(k==0){
-                return x;
-            }
-        }
-        return 0;
+        kth(root, set);
+        auto it = set.begin();
+        advance(it, k-1);
+
+        return *it;
     }
-    void kth(TreeNode* root, set<int>& set){
-        if(root==nullptr){
+    void kth(TreeNode* root, set<int>& set) {
+        if (root == nullptr) {
             return;
         }
         set.insert(root->val);
-        kth(root->left,set);
-        kth(root->right,set);
+        kth(root->left, set);
+        kth(root->right, set);
     }
 };
