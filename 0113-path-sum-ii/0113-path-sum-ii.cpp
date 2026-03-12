d2215aeb2686123be4a9d17dbfff63e80ddd0119
Ayush Sharma <140237136+AyuSharma176@users.noreply.github.com>
2026-03-12 15:45:21 +0530

    Time: 0 ms (100%), Space: 20.9 MB (88.41%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -6,7 +6,8 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
@@ -14,23 +15,22 @@ public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>> result;
         vector<int> l;
-        haspathsum(root,targetSum,l,result);
+        haspathsum(root, targetSum, l, result);
         return result;
     }
-    void haspathsum(TreeNode* root, int target,vector<int>& l,vector<vector<int>>& result){
-        if(root==nullptr){
+    void haspathsum(TreeNode* root, int target, vector<int>& l,
+                    vector<vector<int>>& result) {
+        if (root == nullptr) {
             return;
         }
         l.push_back(root->val);
-        if(root->left==nullptr && root->right==nullptr){
-           if(target==root->val){
-            result.push_back(l);
-            
-           }
-           
+        if (root->left == nullptr && root->right == nullptr) {
+            if (target == root->val) {
+                result.push_back(l);
+            }
         }
-        haspathsum(root->left,target-root->val,l,result);
-        haspathsum(root->right,target-root->val,l,result);
+        haspathsum(root->left, target - root->val, l, result);
+        haspathsum(root->right, target - root->val, l, result);
         l.pop_back();
     }
 };
