f99492ae6bf91bd28d15cf72b61774b3af446a64
Ayush Sharma <140237136+AyuSharma176@users.noreply.github.com>
2026-03-13 21:08:06 +0530

    Time: 28 ms (48.99%), Space: 23.3 MB (27.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int count=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                if(grid[i][j]=='1'){
-                    count++;
-                    dfs(grid,i,j);
+        const int rows = grid.size();
+        const int cols = grid[0].size();
+        int islands = 0;
+
+        static constexpr int DIRS[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if (grid[i][j] != '1')
+                    continue;
+                
+                islands++;
+                grid[i][j] = '0';
+
+                queue<pair<int,int>> q;
+                q.emplace(i,j);
+
+                while(!q.empty()){
+                    auto [currRow, currCol] = q.front();
+                    q.pop();
+
+                    for(const auto& d : DIRS){
+                        int nextRow = currRow + d[0];
+                        int nextCol = currCol + d[1];
+
+                        if (nextRow < 0 || nextRow >= rows || nextCol < 0 || nextCol >= cols || grid[nextRow][nextCol] != '1')
+                            continue;
+                        
+                        grid[nextRow][nextCol] = '0';
+                        q.emplace(nextRow, nextCol);
+                    }
                 }
             }
         }
-        return count;
-    }
-    void dfs(vector<vector<char>>& grid,int i,int j){
-        if(i<0 || j<0 || i>=grid.size()|| j>=grid[i].size()||grid[i][j]=='0'){
-            return;
-        }
-        grid[i][j]='0';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+
+        return islands;
     }
 };
+// auto init = atexit([]() { ofstream("display_runtime.txt") << "0";});
